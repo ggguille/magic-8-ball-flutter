accd2b162968cc6f03e92d819f39b086084bb845
ggguille <999uille@gmail.com>
2020-07-25 17:48:08 +0200

    Build AskMeAnything app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Center(child: Ball()),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int _ballNumber = 1;
+
+  void _ballPressed() {
+    print('Ball Pressed');
+    setState(() {
+      _ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: _ballPressed,
+      child: Image.asset('images/ball$_ballNumber.png'),
+    );
+  }
+}
